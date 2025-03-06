8991212c513f473970f02c76435a226c7d855aa6
terrypacker <terrypacker@users.noreply.github.com>
2025-03-05 16:09:29 -1000

    Fix for flaky BBMDTest
    As per MANGO-1975

--- src/test/java/com/serotonin/bacnet4j/npdu/ip/BBMDTest.java
+++ src/test/java/com/serotonin/bacnet4j/npdu/ip/BBMDTest.java
@@ -6,6 +6,7 @@ import com.serotonin.bacnet4j.event.DeviceEventAdapter;
 import com.serotonin.bacnet4j.service.unconfirmed.WhoIsRequest;
 import com.serotonin.bacnet4j.transport.DefaultTransport;
 import com.serotonin.bacnet4j.util.sero.ByteQueue;
+import com.serotonin.warp.TestingWarpScheduledExecutorService;
 import com.serotonin.warp.WarpClock;
 import org.apache.commons.lang3.mutable.MutableInt;
 import org.junit.After;
@@ -25,6 +26,41 @@ import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * To run on MacOS you need these addresses setup:
+ * sudo ifconfig lo0 alias 127.0.1.0
+ * sudo ifconfig lo0 alias 127.0.1.1
+ * sudo ifconfig lo0 alias 127.0.1.2
+ * sudo ifconfig lo0 alias 127.0.1.3
+ * sudo ifconfig lo0 alias 127.0.1.255
+ *
+ * sudo ifconfig lo0 alias 127.0.2.0
+ * sudo ifconfig lo0 alias 127.0.2.1
+ * sudo ifconfig lo0 alias 127.0.2.2
+ * sudo ifconfig lo0 alias 127.0.2.3
+ * sudo ifconfig lo0 alias 127.0.2.255
+ *
+ * sudo ifconfig lo0 alias 127.0.3.0
+ * sudo ifconfig lo0 alias 127.0.3.1
+ * sudo ifconfig lo0 alias 127.0.3.2
+ * sudo ifconfig lo0 alias 127.0.3.3
+ * sudo ifconfig lo0 alias 127.0.3.255
+ *
+ * sudo ifconfig lo0 alias 127.0.151.0
+ * sudo ifconfig lo0 alias 127.0.151.1
+ * sudo ifconfig lo0 alias 127.0.151.255
+ *
+ * sudo ifconfig lo0 alias 127.0.152.0
+ * sudo ifconfig lo0 alias 127.0.152.1
+ * sudo ifconfig lo0 alias 127.0.152.255
+ *
+ * sudo ifconfig lo0 alias 127.0.153.0
+ * sudo ifconfig lo0 alias 127.0.153.1
+ * sudo ifconfig lo0 alias 127.0.153.255
+ *
+ *
+ * sudo ifconfig lo0 alias 127.0.254.254
+ */
 public class BBMDTest {
     static final int port = 0xBAC0;
 
@@ -116,6 +152,10 @@ public class BBMDTest {
         ld33.ld.sendLocalBroadcast(new WhoIsRequest());
         clock.plus(400, TimeUnit.MILLISECONDS, 800);
 
+        ld11.executor.waitForExecutorTasks(10, TimeUnit.MILLISECONDS, 50, true, true, false);
+        ld12.executor.waitForExecutorTasks(10, TimeUnit.MILLISECONDS, 50, true, true, false);
+        ld33.executor.waitForExecutorTasks(10, TimeUnit.MILLISECONDS, 50, true, true, false);
+
         // Confirm that the above broadcasts were only received on their subnets.
         assertEquals(1, ld11.iamCount()); // IAm from 12
         assertEquals(1, ld12.iamCount()); // IAm from 11
@@ -196,6 +236,7 @@ public class BBMDTest {
 
         // Verify that it is there
         configurer.send(packet(6, "", ld11));
+        ld11.executor.waitForExecutorTasks(10, TimeUnit.MILLISECONDS, 50, true, true, false);
         configurer.receive(response);
         assertPacketEquals("810700227F009701BAC0000300207F009801BAC0001E003B7F009901BAC00001001F", response);
 
@@ -205,6 +246,7 @@ public class BBMDTest {
 
         // Read registrations again
         configurer.send(packet(6, "", ld11));
+        ld11.executor.waitForExecutorTasks(10, TimeUnit.MILLISECONDS, 50, true, true, false);
 
         // Get the response
         configurer.receive(response);
@@ -470,8 +512,8 @@ public class BBMDTest {
                 .withLocalNetworkNumber(1) //
                 .build();       
         info.network.enableBBMD();
-
-        info.ld = new LocalDevice(subnet * 10 + addr, new DefaultTransport(info.network), clock) //
+        info.executor = new TestingWarpScheduledExecutorService(clock);
+        info.ld = new LocalDevice(subnet * 10 + addr, new DefaultTransport(info.network), clock, info.executor) //
                 .initialize();
 
         info.iamCount = new MutableInt();
@@ -513,6 +555,7 @@ public class BBMDTest {
         LocalDevice ld;
         IpNetwork network;
         MutableInt iamCount;
+        TestingWarpScheduledExecutorService executor;
 
         void reset() {
             ld.clearRemoteDevices();
