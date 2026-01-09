f2602aadf9e9b2eb6f57690c3def1d51c9922fc8
Matthew Lohbihler <matthew@serotonin.ai>
2026-01-09 15:52:41 -0500

    revert BACnet to BAcnet

--- src/main/java/com/serotonin/bacnet4j/RemoteDevice.java
+++ src/main/java/com/serotonin/bacnet4j/RemoteDevice.java
@@ -19,7 +19,7 @@
  *
  * When signing a commercial license with Radix IoT LLC,
  * the following extension to GPL is made. A special exception to the GPL is
- * included to allow you to distribute a combined work that includes BACnet4J
+ * included to allow you to distribute a combined work that includes BAcnet4J
  * without being obliged to provide the source code for any proprietary components.
  *
  * See www.radixiot.com for commercial license options.
