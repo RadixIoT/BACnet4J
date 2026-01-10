cf170190fb03f47c49456660be4e67f22e256e13
Matthew Lohbihler <matthew@serotonin.ai>
2026-01-09 20:25:34 -0500

    add copyright

--- src/main/java/com/serotonin/bacnet4j/type/primitive/encoding/Iso8859CharacterEncoder.java
+++ src/main/java/com/serotonin/bacnet4j/type/primitive/encoding/Iso8859CharacterEncoder.java
@@ -1,3 +1,30 @@
+/*
+ * ============================================================================
+ * GNU General Public License
+ * ============================================================================
+ *
+ * Copyright (C) 2025 Radix IoT LLC. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * When signing a commercial license with Radix IoT LLC,
+ * the following extension to GPL is made. A special exception to the GPL is
+ * included to allow you to distribute a combined work that includes BAcnet4J
+ * without being obliged to provide the source code for any proprietary components.
+ *
+ * See www.radixiot.com for commercial license options.
+ */
+
 package com.serotonin.bacnet4j.type.primitive.encoding;
 
 import static com.serotonin.bacnet4j.type.primitive.encoding.StandardCharacterEncodings.ISO_8859_1;
